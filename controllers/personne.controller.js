f48306df0b810245eed7831605bc822032024e6a
achreftun <elmouelhi.achref@gmail.com>
2025-09-02 13:40:08 +0200

    formulaire+ session

---
+++ controllers/personne.controller.js
@@ -0,0 +1,30 @@
+const personnes = [
+    { id: 1, nom: "Wick", prenom: "John", age: 45 },
+    { id: 2, nom: "Dalton", prenom: "Jack", age: 55 },
+    { id: 3, nom: "Maggio", prenom: "Sophie", age: 33 },
+]
+
+const showPersonnes = (req, res, next) => {
+    res.render('personne', {
+        personnes
+    })
+}
+const addPersonne = (req, res, next) => {
+    personnes.push(req.body)
+    req.session.firstname = req.body.prenom
+    res.redirect('/personne')
+
+}
+const deletePersonne = (req, res, next) => {
+    const id = req.params.id
+    const index = personnes.findIndex(p => p.id == id)
+    if (index != -1) {
+        personnes.splice(index, 1)
+    } else {
+        alert("Suppression impossible !")
+    }
+    res.redirect('/personne')
+
+}
+
+export default { showPersonnes, addPersonne, deletePersonne }
